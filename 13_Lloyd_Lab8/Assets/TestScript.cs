1ccc011dcc5d36ffba5f8383ac72f90029bdb3b4
FriedPotatoe <lloydlim1@gmail.com>
2020-11-09 16:20:42 +0800

    main

---
+++ 13_Lloyd_Lab8/Assets/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+    float spaceBarCounter=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "Score: "+spaceBarCounter;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceBarCounter++;
+
+        }
+    }
+}
